21b877e55903b920c7323b17666851139a6480eb
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-07 14:42:14 +0800

    Integrate editorial interlude and motion

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from './components/layout/Header'
 import { Footer } from './components/layout/Footer'
 import { Hero } from './components/home/Hero'
 import { PerformanceStats } from './components/home/PerformanceStats'
+import { EditorialInterlude } from './components/home/EditorialInterlude'
 import { AboutMarci } from './components/home/AboutMarci'
 import { PropertySearch } from './components/home/PropertySearch'
 import { SellerExperience } from './components/home/SellerExperience'
@@ -9,8 +10,11 @@ import { BuyerExperience } from './components/home/BuyerExperience'
 import { PropertyGallery } from './components/home/PropertyGallery'
 import { Services } from './components/home/Services'
 import { Contact } from './components/home/Contact'
+import { useEditorialMotion } from './hooks/useEditorialMotion'
 
 export default function App() {
+  useEditorialMotion()
+
   return (
     <>
       <a className="skip-link" href="#main-content">Skip to content</a>
@@ -18,6 +22,7 @@ export default function App() {
       <main id="main-content" tabIndex={-1}>
         <Hero />
         <PerformanceStats />
+        <EditorialInterlude />
         <AboutMarci />
         <PropertySearch />
         <SellerExperience />
