162a59ef4cd3ad6735121c21e81dab270a0d4f22
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-07 16:48:06 +0800

    Load cinematic hero motion styling

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './styles/2026-editorial-v3.css'
 import './styles/2026-cleanup.css'
 import './styles/about-profile.css'
 import './styles/property-search.css'
+import './styles/hero-motion.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
