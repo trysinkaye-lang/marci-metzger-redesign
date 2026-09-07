81d2c884b53fc0646362c193f2033e66f7b40596
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-07 13:39:31 +0800

    Load 2026 editorial styling after base styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tokens.css'
 import './styles/globals.css'
+import './styles/2026-editorial.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
