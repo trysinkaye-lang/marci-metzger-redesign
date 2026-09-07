ccd62e1b13c57e06653c6e84402b00fe4207ab6b
trysinkaye-lang <trysinkaye@gmail.com>
2026-09-07 17:34:31 +0800

    Load buyer story styling

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './styles/about-profile.css'
 import './styles/property-search.css'
 import './styles/hero-motion.css'
 import './styles/services-motion.css'
+import './styles/buyer-story.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
